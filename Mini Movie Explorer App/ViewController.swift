422f29275de766d8308be52da14134086b68194d
Mostafa Hendawi <mostafawael7@hotmail.com>
2025-06-17 13:27:57 +0300

    Initial Commit

---
+++ Mini Movie Explorer App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini Movie Explorer App
+//
+//  Created by Mostafa Hendawi on 17/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
